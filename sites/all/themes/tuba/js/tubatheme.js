8ade36e94fe344da4a58f1a2e60ee1e9e33269d9
Benjamin Christensen <benjamin@benway.dk>
2011-12-29 10:31:12 +0100

    styling and theming fold-out boxes on front page

---
+++ sites/all/themes/tuba/js/tubatheme.js
@@ -0,0 +1,32 @@
+(function($) {
+Drupal.behaviors.myBehavior = {
+  attach: function (context, settings) {
+    
+    //HIDE LATEST MENU CONTENT
+    $("#block-forum-new .content, #block-views-begivenheder-block .content, #calendar_tab_foldout").hide();
+
+    //FORUM Foldout
+    $("#block-forum-new h2").toggle(
+      function(){
+        $("#block-forum-new .content").hide();
+        $("#block-forum-new .content").show();
+      },
+        function(){
+        $("#block-forum-new .content").hide();
+      }
+    )
+    
+    //BREVKASSE Foldout
+    $("#block-views-begivenheder-block h2").toggle(
+      function(){
+        $("#block-views-begivenheder-block .content").hide();
+        $("#block-views-begivenheder-block .content").show();
+      },
+        function(){
+        $("#block-views-begivenheder-block .content").hide();
+      }
+    )
+
+  }
+};
+})(jQuery);
