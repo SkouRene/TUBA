64fe025aae67663af543d7fe2f1f1549505ed616
Benjamin Christensen <benjamin@benway.dk>
2012-01-04 00:10:49 +0100

    Styling front page: latest items fold out and slideshow

--- sites/all/themes/tuba/js/tubatheme.js
+++ sites/all/themes/tuba/js/tubatheme.js
@@ -3,23 +3,23 @@ Drupal.behaviors.myBehavior = {
   attach: function (context, settings) {
     
     //HIDE LATEST MENU CONTENT
-    $("#block-forum-new .content, #block-views-begivenheder-block .content").hide();
+    $("#block-views-forum-block .content, #block-views-begivenheder-block .content").hide();
 
     //FORUM Foldout
-    $("#block-forum-new h2").toggle(
+    $("#block-views-forum-block h2").toggle(
       function(){
-        $("#block-forum-new .content").hide();
-        $("#block-forum-new .content").show();
+        $("#block-views-forum-block .content, #block-views-begivenheder-block .content").hide();
+        $("#block-views-forum-block .content").show();
       },
         function(){
-        $("#block-forum-new .content").hide();
+        $("#block-views-forum-block .content").hide();
       }
     )
     
     //BREVKASSE Foldout
     $("#block-views-begivenheder-block h2").toggle(
       function(){
-        $("#block-views-begivenheder-block .content").hide();
+        $("#block-views-forum-block .content, #block-views-begivenheder-block .content").hide();
         $("#block-views-begivenheder-block .content").show();
       },
         function(){
